a04ea833b42053779455d1b27ddec1517784969a
Pranesh Pandurangan <praneshpg@gmail.com>
2014-06-20 17:39:50 -0700

    Finish changes to stop an engine
    Added a call to stop all react scripts, by passing all known routing
    keys to the react_killer function. Then stop the threadpool executor.
    Finally, Raise a known exception to stop the watchdog thread. This
    will throw an ugly traceback, but will shutdown the engine
    gracefully.
    
    Also made minor changes to the example react.json, to change the log
    format. Knowing the time a log was printed is useful.
    
    Change-Id: Ibed06f79547312d188feb499f937eb5390d60c3e

--- entropy/utils.py
+++ entropy/utils.py
@@ -110,6 +110,7 @@ def watch_dir_for_change(dirs_to_watch, event_fn):
     observer = Observer()
     for directory in dirs_to_watch:
         observer.schedule(event_handler, path=directory)
+    observer.setDaemon(True)
     observer.start()
     return observer
 
